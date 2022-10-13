2206f059585407e0597906ca760777d7367f1063
VIVIN AATHAV URK21CS1154 <110010382+Vivin-1154@users.noreply.github.com>
2022-10-13 21:11:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ for item in input().split(', '):
   nums1.append(int(item))
 for item in input().split(', '):
   nums2.append(int(item))
-m = int(input())
+m = int(input()
 n = int(input())
 merge(nums1, m, nums2, n
 print(nums1)
